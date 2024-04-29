ccb0d4f2a098b49162035bf16f03dc45ce992a3d
ALLEE001 <alexllee@umich.edu>
2024-04-28 23:10:08 -0400

    Class GetUpdateData: bug fix

--- src/get_data.py
+++ src/get_data.py
@@ -8,38 +8,46 @@ from datetime import date, datetime, timedelta
 import random
 import os
 
+# TODO Handle update_existing=False, existing_data_path=True cases
 class GetUpdateData:
     """Get data for new tickers, or update an existing data with new dates using Twelve Data's API. This class works for free version."""
     def __init__(self, update_existing=True, existing_data_path=None, api_key=None):
         self.update_existing = update_existing
         self.existing_data_path = existing_data_path
         self.api_key = api_key
-        try:
-            self.existing_data = (pd.read_csv(self.existing_data_path)
-                                  .sort_values(['ticker', 'datetime'], ascending=False)
-                                  .drop_duplicates(subset='ticker'))
-        except:
-            pass
+        
+        if update_existing:
+            try:
+                self.existing_data = (pd.read_csv(self.existing_data_path)
+                                    .sort_values(['ticker', 'datetime'], ascending=False)
+                                    .drop_duplicates(subset='ticker'))
+            except:
+                raise ValueError("existing_data_path is either incorrect or undefined.")
+
 
     def get_tickers(self):
         """Get S&P 500 tickers from wikipedia. Get tickers in existing data if updating instead."""
-        
-        df_tickers = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
-        df_tickers['Symbol'] = np.where(df_tickers['Symbol'].isin(['GOOG', 'GOOGL']), 'GOOGL', df_tickers['Symbol'])
-        df_tickers.drop_duplicates(subset='Symbol', inplace=True)
-        
+
         if self.update_existing:
+            df_tickers = self.existing_data[['ticker', 'Security', 'GICS Sector', 'GICS Sub-Industry', 'Headquarters Location', 'Date added', 'CIK', 'Founded']]
             tickers = list(self.existing_data['ticker'])
         else:
+            
+            df_tickers = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+            df_tickers = df_tickers.rename(columns={'Symbol':'ticker'})
+            df_tickers['ticker'] = np.where(df_tickers['ticker'].isin(['GOOG', 'GOOGL']), 'GOOGL', df_tickers['ticker'])
+            df_tickers.drop_duplicates(subset='ticker', inplace=True)
+            
             try:
-                df_tickers = df_tickers[~df_tickers['Symbol'].isin(self.existing_data['ticker'])]
+                df_tickers = df_tickers[~df_tickers['ticker'].isin(self.existing_data['ticker'])]
             except:
                 pass
             
             limit = min(200, len(df_tickers))
-            tickers = list(df_tickers.iloc[0:limit, df_tickers.columns.get_loc('Symbol')])
+            tickers = list(df_tickers.iloc[0:limit, df_tickers.columns.get_loc('ticker')])
 
         return tickers, df_tickers
+    
     
     def get_data_new(self):
         """Get new ticker data from twelve data."""
@@ -122,7 +130,7 @@ class GetUpdateData:
         for ticker in tickers:
                 
             try:
-                start_date = (pd.to_datetime(self.existing_data[self.existing_data['ticker']==ticker]['datetime']) + timedelta(days=1)).dt.strftime('%Y-%m-%d')
+                start_date = (pd.to_datetime(self.existing_data[self.existing_data['ticker']==ticker]['datetime']) + timedelta(days=1)).dt.strftime('%Y-%m-%d').values[0]
                 end_date = date.today().strftime('%Y-%m-%d')
                 
                 url = f'https://api.twelvedata.com/time_series?&start_date={start_date}&end_date={end_date}&symbol={ticker}&format=CSV&interval=1day&apikey={self.api_key}'
@@ -147,11 +155,15 @@ class GetUpdateData:
         
         return data_frames
         
-        
+         
     def download_data(self):
         if self.update_existing:
+            print(f'Getting updates...')
+            print(f'This may take a long time (1H) due to api limit.')
             data_frames = self.get_data_updates()
         else:
+            print(f'Getting data for new tickers...')
+            print(f'This may take a long time (1H) due to api limit.')
             data_frames = self.get_data_new()
             
         df_final = pd.concat([i for i in data_frames if len(i) > 0])
@@ -159,11 +171,22 @@ class GetUpdateData:
         _, df_tickers = self.get_tickers()
         df_final = df_final.merge(df_tickers, on='ticker', how='left')
 
-        # Only save tickers with 5 years or more data
-        ticker_days = df_final.groupby('ticker')['ticker'].value_counts().reset_index()
-        five_yrs_more = ticker_days[ticker_days['count'] >= 1825]['ticker']
-        df_final = df_final[df_final['ticker'].isin(five_yrs_more)]
-
-        df_final.to_csv(f'data/raw/{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.csv', index=False)
+        path = f'data/raw/{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.csv'
+        df_final.to_csv(path, index=False)
+        print(f'Download completed to {path}')
         
-GetUpdateData(update_existing=True, existing_data_path='data/data.csv', api_key=api).download_data()
+        
+    def merge_files(self):
+        # List files in the directory
+        directory = 'data/raw/'
+        files = os.listdir(directory)
+
+        l_all_raw = []
+        for i in files:
+            l_all_raw.append(pd.read_csv(f'{directory}{i}'))
+
+        df_out = pd.concat(l_all_raw).drop_duplicates(subset=['datetime', 'ticker']) 
+
+        path = 'data/data.csv'
+        df_out.to_csv(path, index_label=False)
+        print(f'Merged all raw files to {path}')
